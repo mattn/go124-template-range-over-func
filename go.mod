0efb985575ac5a6a71d4d676073609a41c8f718e
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2025-03-01 02:01:29 +0900

    first import

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mattn/go124-template-range-over-func
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"text/template"
+)
+
+const contents = `{{ range $l := .}}{{ $l | trim }}
+{{ end }}`
+
+const lines = `
+xxxx
+yyyy
+zzzz
+`
+
+func main() {
+	tmpl := template.Must(template.New("").Funcs(template.FuncMap{
+		"trim": strings.TrimSpace,
+	}).Parse(contents))
+
+	tmpl.Execute(os.Stdout, strings.Lines(strings.TrimSpace(lines)))
+}
